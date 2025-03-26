21ea68f2f69e81f57f8a23216ff8549b844e0c35
João Mendonça <joao.pedro.mendonca@tecnico.ulisboa.pt>
2025-03-26 08:53:29 +0000

    add site

---
+++ site_project/src/App.vue
@@ -0,0 +1,69 @@
+<script setup>
+import CurrentTime from './components/CurrentTime.vue';
+import RaspberrySelector from './components/RaspberrySelector.vue';
+</script>
+
+<template>
+  <div class="page">
+    <h1>Low Cost Probes for Continuous Monitoring and Optimisation of Wireless Network Performance</h1>
+    <div class="boxes">
+      <div class="box_clock">
+        <CurrentTime />
+      </div>
+      <div class="box_rp">
+        <RaspberrySelector />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.page {
+  min-height: 100vh; /* Garante que a página ocupe toda a altura */
+  background-color: #17072b; /* Fundo da página */
+  text-align: center; /* Centraliza o texto */
+  padding: 20px; /* Espaçamento interno */
+}
+
+
+h1 {
+  color: #FFFFFF;
+  font-size: 50px;
+  margin-bottom: 50px;
+  font-family: 'Roboto', sans-serif;
+}
+
+.boxes {
+  display: flex; /* Coloca as caixas lado a lado */
+  justify-content: flex-start;
+  gap: 20px;
+  flex-wrap: wrap;
+}
+
+
+.box_clock{
+  width: 300px;
+  padding: 15px;
+  border-radius: 25px;
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.1);
+  background: #fff;
+}
+
+.box_rp {
+  width: 300px;
+  height: 120px;
+  padding: 15px;
+  border-radius: 25px;
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.1);
+  background: #fff;
+}
+
+</style>
+
+<style>
+html, body {
+  background-color: #17072b;
+  margin: 0;
+  padding: 0;
+}
+</style>
---
+++ site_project/src/components/CurrentTime.vue
@@ -0,0 +1,45 @@
+<!-- src/components/CurrentTime.vue -->
+<script setup>
+import { ref, onMounted } from 'vue';
+
+const currentTime = ref(new Date().toLocaleString());
+
+onMounted(() => {
+  setInterval(() => {
+    currentTime.value = new Date().toLocaleString();
+  }, 1000);
+});
+</script>
+
+<template>
+  <div class="time-container">
+    <p class="welcome-message">Welcome</p>
+    <div class="time">
+      <p>{{ currentTime }}</p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.time-container {
+  text-align: center;
+}
+
+.welcome-message {
+  font-size: 25px;
+  font-weight: bold;
+  color: #333;
+  margin-bottom: 5px;
+  font-family: 'Roboto', sans-serif;
+}
+
+.time {
+  font-size: 30px;
+  color: #FFFFFF;
+  font-family: 'Roboto', sans-serif;
+  background: rgba(36, 60, 230, 0.8);
+  padding: 3px;
+  border-radius: 25px;
+  display: inline-block;
+}
+</style>
