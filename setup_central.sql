62241ab87adf3098176ffa3121c890ede32727ae
Joao Simoes <joaoasimoes@tecnico.ulisboa.pt>
2025-06-06 02:04:19 +0100

    feat: implementar um trigger na tabela dos dados_rede para inserir na tabela dos erros

--- setup_central.sql
+++ setup_central.sql
@@ -2,8 +2,8 @@ CREATE DATABASE IF NOT EXISTS central_monitoramento;
 
 USE central_monitoramento;
 
-DROP TABLE IF EXISTS logs;
 DROP TABLE IF EXISTS dados_rede;
+DROP TABLE IF EXISTS erros_dados_rede;
 DROP TABLE IF EXISTS all_aps;
 DROP TABLE IF EXISTS raspberrypis;
 
@@ -14,28 +14,36 @@ CREATE TABLE IF NOT EXISTS raspberrypis (
     localizacao VARCHAR(50)
 );
 
--- CREATE TABLE IF NOT EXISTS logs (
---     id INT AUTO_INCREMENT PRIMARY KEY,
---     raspberrypi_id INT NOT NULL,  -- ID do Raspberry Pi (referência à tabela raspberrypis)
---     mensagem TEXT,       -- Mensagem do log
---     timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,  -- Hora de inserção do log
---     FOREIGN KEY (raspberrypi_id) REFERENCES raspberrypis(id) -- Relacionamento com raspberrypis
--- );
-
 CREATE TABLE IF NOT EXISTS dados_rede (
-    id INT AUTO_INCREMENT PRIMARY KEY,  -- Identificador único
-    raspberrypi_id INT,  -- Identificador do Raspberry Pi
-    timestamp DATETIME NOT NULL,  -- Data e hora da coleta
-    latencia_ms FLOAT,  -- Latência em milissegundos
-    perda_pacotes INT,  -- Perda de pacotes em porcentagem
-    download_mbps FLOAT,  -- Velocidade de download em Mbps
-    upload_mbps FLOAT,  -- Velocidade de upload em Mbps
-    rtt_min FLOAT,  -- RTT mínimo em milissegundos
-    rtt_avg FLOAT,  -- RTT médio em milissegundos
-    rtt_max FLOAT,  -- RTT máximo em milissegundos
-    rtt_mdev FLOAT,  -- Desvio padrão do RTT
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    raspberrypi_id INT,
+    timestamp DATETIME NOT NULL,
+    latencia_ms FLOAT,
+    perda_pacotes INT,
+    download_mbps FLOAT,
+    upload_mbps FLOAT,
+    rtt_min FLOAT,
+    rtt_avg FLOAT,
+    rtt_max FLOAT,
+    rtt_mdev FLOAT,
     num_aps INT,
-    FOREIGN KEY (raspberrypi_id) REFERENCES raspberrypis(id)  -- Relacionamento com a tabela raspberrypis
+    FOREIGN KEY (raspberrypi_id) REFERENCES raspberrypis(id)
+);
+
+CREATE TABLE IF NOT EXISTS erros_dados_rede (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    raspberrypi_id INT,
+    timestamp DATETIME NOT NULL,
+    latencia_ms FLOAT,
+    perda_pacotes INT,
+    download_mbps FLOAT,
+    upload_mbps FLOAT,
+    rtt_min FLOAT,
+    rtt_avg FLOAT,
+    rtt_max FLOAT,
+    rtt_mdev FLOAT,
+    num_aps INT,
+    FOREIGN KEY (raspberrypi_id) REFERENCES raspberrypis(id)
 );
 
 CREATE TABLE IF NOT EXISTS all_aps (
@@ -46,7 +54,7 @@ CREATE TABLE IF NOT EXISTS all_aps (
   bssid VARCHAR(50),
   rate INT,
   sig INT,
-  FOREIGN KEY (raspberrypi_id) REFERENCES raspberrypis(id)  -- Relacionamento com a tabela raspberrypis
+  FOREIGN KEY (raspberrypi_id) REFERENCES raspberrypis(id)
 );
 
 CREATE USER IF NOT EXISTS 'monitor'@'%' IDENTIFIED BY 'senha_segura';
@@ -54,3 +62,45 @@ CREATE USER IF NOT EXISTS 'monitor'@'%' IDENTIFIED BY 'senha_segura';
 GRANT ALL PRIVILEGES ON central_monitoramento.* TO 'monitor'@'%';
 
 FLUSH PRIVILEGES;
+
+DELIMITER //
+
+CREATE TRIGGER trg_erros_na_rede
+AFTER INSERT ON dados_rede
+FOR EACH ROW
+BEGIN
+  IF NEW.latencia_ms > 50.0
+  OR NEW.perda_pacotes > 2
+  OR NEW.download_mbps < 10.0
+  OR NEW.upload_mbps < 10.0
+  OR NEW.rtt_avg > 200.0 THEN
+    INSERT INTO erros_dados_rede (
+    raspberrypi_id,
+    timestamp,
+    latencia_ms,
+    perda_pacotes,
+    download_mbps,
+    upload_mbps,
+    rtt_min,
+    rtt_avg,
+    rtt_max,
+    rtt_mdev,
+    num_aps
+    )
+    VALUES (
+    NEW.raspberrypi_id,
+    NEW.timestamp,
+    NEW.latencia_ms,
+    NEW.perda_pacotes,
+    NEW.download_mbps,
+    NEW.upload_mbps,
+    NEW.rtt_min,
+    NEW.rtt_avg,
+    NEW.rtt_max,
+    NEW.rtt_mdev,
+    NEW.num_aps
+    );
+  END IF;
+END;//
+
+DELIMITER ;
