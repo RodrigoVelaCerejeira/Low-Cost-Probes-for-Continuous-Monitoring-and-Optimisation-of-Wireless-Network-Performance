50333be6e3cfe9f6f270531c61cd0980c9765399
João Mendonça <joao.pedro.mendonca@tecnico.ulisboa.pt>
2025-06-15 15:57:59 +0100

    correção de uma cgada que fize :=0

--- site_project/frontend/src/views/DashbordView.vue
+++ site_project/frontend/src/views/DashbordView.vue
@@ -67,13 +67,6 @@
 
       <div class="mt-4 flex justify-end">
 
-        <button @click="exportData" class="px-6 py-2 rounded-lg shadow transition duration-300 ease-in-out
-            hover:scale-105
-            disabled:bg-transparent disabled:text-gray-400 disabled:border disabled:border-gray-400
-            bg-indigo-600 text-white hover:bg-indigo-700 mr-8">
-          Export Data
-        </button>
-
         <button @click="openPopup(selectedRaspberries[0])" :disabled="selectedRaspberries.length !== 1" class="px-6 py-2 rounded-lg shadow transition duration-300 ease-in-out
             hover:scale-105
             disabled:bg-transparent disabled:text-gray-400 disabled:border disabled:border-gray-400
@@ -107,14 +100,13 @@
         <div v-else class="px-6 py-4">
           <h3 class="text-lg font-medium mb-2">Failures last hour:</h3>
           <ul v-if="Object.keys(selectedFailures).length > 0">
-            <li v-for="failureType in Object.keys(selectedFailures)" :key="failureType">
+            <li v-for="failure in selectedFailures" :key="failureType">
               <strong>
-                <template v-if="failureType === 'latency'">High Latency</template>
-                <template v-else-if="failureType === 'download_speed'">Low Download Speed</template>
-                <template v-else-if="failureType === 'upload_speed'">Low Upload Speed</template>
-                <template v-else-if="failureType === 'packet_loss'">Packet Loss</template>
-                <template v-else-if="failureType === 'round_trip_time'">High RTT</template>
-                <template v-else>{{ failureType }}</template>
+                <template v-if="failure === 1">High Latency</template>
+                <template v-else-if="failure === 2">Low Download Speed</template>
+                <template v-else-if="failure === 3">Low Upload Speed</template>
+                <template v-else-if="failure === 4">Packet Loss</template>
+                <template v-else-if="failure === 5">High RTT</template>
               </strong>
             </li>
           </ul>
@@ -122,14 +114,13 @@
           <br>
           <h3 class="text-lg font-medium mb-2">Failures last day:</h3>
           <ul v-if="Object.keys(selectedFailuresDay).length > 0">
-            <li v-for="failureType in Object.keys(selectedFailuresDay)" :key="failureType">
+            <li v-for="failure in Object.keys(selectedFailuresDay)" :key="failureType">
               <strong>
-                <template v-if="failureType === 'latency'">High Latency</template>
-                <template v-else-if="failureType === 'download_speed'">Low Download Speed</template>
-                <template v-else-if="failureType === 'upload_speed'">Low Upload Speed</template>
-                <template v-else-if="failureType === 'packet_loss'">Packet Loss</template>
-                <template v-else-if="failureType === 'round_trip_time'">High RTT</template>
-                <template v-else>{{ failureType }}</template>
+                <template v-if="failure === 1">High Latency</template>
+                <template v-else-if="failure === 2">Low Download Speed</template>
+                <template v-else-if="failure === 3">Low Upload Speed</template>
+                <template v-else-if="failure === 4">Packet Loss</template>
+                <template v-else-if="failure === 5">High RTT</template>
               </strong>
             </li>
           </ul>
@@ -157,7 +148,11 @@
         <tbody class="divide-y divide-gray-300 text-gray-800">
           <tr v-for="f in failures" :key="f">
             <td class="px-6 py-4">{{ f.raspberrypi_id }}</td>
-            <td class="px-6 py-4">Mensagem a definir com o Joao</td>
+            <td class="px-6 py-4" v-if="f.failure === 1">High Latency</td>
+            <td class="px-6 py-4" v-else-if="f.failure === 2">Low Download Speed</td>
+            <td class="px-6 py-4" v-else-if="f.failure === 3">Low Upload Speed</td>
+            <td class="px-6 py-4" v-else-if="f.failure === 4">Packet Loss</td>
+            <td class="px-6 py-4" v-else-if="f.failure === 5">High RTT</td>
             <td class="px-6 py-4">{{ f.timestamp }}</td>
           </tr>
         </tbody>
@@ -303,6 +298,7 @@ const selectedFailures = ref({});
 const selectedFailuresDay = ref({});
 
 async function openPopup(raspberryId) {
+  selectedFailures.value = await getSpecificFailures(raspberryId, "hour");
   selectedFailuresDay.value = await getSpecificFailures(raspberryId, "day");
   console.log("selectedFailures:", selectedFailures.value);
   isPopupVisible.value = true;
